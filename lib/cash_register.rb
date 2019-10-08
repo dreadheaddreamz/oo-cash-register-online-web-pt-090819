5b8fb5ed39142ec8de67e48be42a768809cc2d36
James Batsell <jamesbatsell05@gmail.com>
2019-10-08 19:18:33 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -9,7 +9,7 @@ class CashRegister
    end
   
   def add_item(title,price, quanity=1)
-    self.total += (price * quanity)
+    self.total += price * quanity
     self.last_trans = self.total
     quanity.times do
       @items << title
